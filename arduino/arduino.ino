4dc682daf99cabf56ae0bf83f735eb5f591020f0
boat-bold647 <fanihossein647@gmail.com>
2023-04-06 19:47:02 +0330

    add sample arduino code

---
+++ arduino/arduino.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(0);
+  delay(700);
+  Serial.println(70);
+  delay(700);
+  Serial.println(130);
+  delay(700);
+  Serial.println(255);
+  delay(700);
+}
